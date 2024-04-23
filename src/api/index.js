a369beb99e33501c28b7676a01e992262b0fb3f0
daudonhere <hellodadedaud#gmail.com>
2024-04-23 12:58:39 +0700

    add constant endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,5 @@
-export const API_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}`;
+export const USER_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/users`;
+export const ALBUMS_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/albums`;
+export const POST_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/posts`;
+export const COMMENT_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/comments`;
+export const PHOTO_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/photos`;
